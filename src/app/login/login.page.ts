31250ca235713f91094225086824225e16e22990
Jaurbyr31 <125709995+Jaurbyr31@users.noreply.github.com>
2023-10-12 20:41:15 -0500

    Add files via upload

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { HttpClient } from '@angular/common/http';
-import { NavController } from '@ionic/angular';
-
 
 @Component({
   selector: 'app-login',
@@ -11,30 +9,56 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  public username: string = '';
-  public password: string = '';
-  constructor(
-    private router: Router
-  ){}
+  username: string = '';
+  password: string = '';
+  
+  users: any = [];
+
+
+  constructor( private http: HttpClient, private router: Router){this.getUser();}
 
   ngOnInit() {}
+
+  getUser() {
+    this.http.get('http://localhost/iumecobd/user.php').subscribe(
+      (response) => {
+        console.log('Respuesta del servidor:', response);
+        this.users = response;
+      },
+      (error) => {
+        console.error('Error al obtener datos del servidor:', error);
+      }
+    );
+  }
+
+  
   
   /*Aca esta la funcoin para el ingreso a la paguina, 
   con las credenciales juan para instructor con su respectiva contraseña y juancho para coordinador con su respectiva contraseña*/
   login() {
-    const validUsername1 = 'juan'; // Nombre de usuario válido
-    const validPassword1 = '8'; // Contraseña válida
-    const validUsername2 = 'juancho'; // Nombre de usuario válido
-    const validPassword2 = '88'; // Contraseña válida
-
-    if (this.username === validUsername1 && this.password === validPassword1) {
-      this.router.navigate(['/home']);
-    } else {
-      if (this.username === validUsername2 && this.password === validPassword2) {
-      this.router.navigate(['/home-coordinador']);
+    // Obtener las credenciales ingresadas por el usuario
+    const inputUsername = this.username;
+    const inputPassword = this.password;
+  
+    // Buscar si las credenciales coinciden con algún usuario en this.users
+    const user = this.users.find((u: any) => u.user === inputUsername && u.contra === inputPassword);
+  
+    if (user) {
+      // Las credenciales son correctas, verificar el valor del campo "cargo"
+      if (user.cargo === 'instructor') {
+        // Usuario instructor
+        this.router.navigate(['/home']);
+      } else if (user.cargo === 'coordinador') {
+        // Usuario coordinador
+        this.router.navigate(['/home-coordinador']);
       } else {
-        alert('Credenciales incorrectas intente nuevamente');
+        // Cargo desconocido
+        alert('Credenciales incorrectas, intente nuevamente');
       }
+    } else {
+      // Las credenciales son incorrectas
+      alert('Credenciales incorrectas, intente nuevamente');
     }
   }
+  
 }
